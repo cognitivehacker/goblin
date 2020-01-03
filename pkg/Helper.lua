0426131734fcfa5d1486935eeedb2169381fa1e3
Georgio <georgio.barbosa@gmail.com>
2020-01-03 06:40:00 -0300

    ArrMap modifies arr and has no return

--- pkg/Helper.lua
+++ pkg/Helper.lua
@@ -1,36 +1,41 @@
 local helper = {}
 
 function helper.Copy(obj, seen)
-    if type(obj) ~= 'table' then return obj end
-    if seen and seen[obj] then return seen[obj] end
-    local s = seen or {}
-    local res = setmetatable({}, getmetatable(obj))
-    s[obj] = res
-    for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
-    return res
+  if type(obj) ~= 'table' then return obj end
+  if seen and seen[obj] then return seen[obj] end
+  local s = seen or {}
+  local res = setmetatable({}, getmetatable(obj))
+  s[obj] = res
+  for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
+  return res
 end
 
 function helper.ArrFilter(arr, func)
-    local out = {}
-    for _, e in ipairs(arr) do
-        if not func(e) then
-            table.insert(out, e)
-        end
+  local out = {}
+  for _, e in ipairs(arr) do
+    if not func(e) then
+      table.insert(out, e)
     end
-    return out
+  end
+  return out
 end
 
+function helper.MicroRand(min, max, precision)
+  precision = precision or 2
+  min = min or 0
+  max = max or 100
+
+  return math.random(min, max) / (10 ^ precision)
+end
 
 function helper.ArrMap(arr, func)
-    local out = {}
-    for _, e in ipairs(arr) do
-        table.insert(out, func(e))
-    end
-    return out
+  for i, e in ipairs(arr) do
+    arr[i] = func(e)
+  end
 end
 
 function helper.Euclid(a, b)
-    return math.sqrt(((b.x - a.x) ^ 2) + ((b.y - a.y) ^ 2));
+  return math.sqrt(((b.x - a.x) ^ 2) + ((b.y - a.y) ^ 2));
 end
 
 return helper
